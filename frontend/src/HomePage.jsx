e8ebfa47276de40904ecf12bcc773aa0a1557c50
User Name <user@gmail.com>
2026-01-28 15:28:37 +0530

    just website setup

---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,25 @@
+export function HomePage() {
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">
+          KANAM 2026 HACKATHON
+        </h1>
+
+      </div>
+      <div className="problem-statement">
+        <p>
+          Smart Greenhouse Climate Analytics
+        </p>
+        <p>
+          Theme Explanation:
+        </p>
+        <p>
+          Uses climate sensors data to regulate temperature, humidity and CO2 levels enhancing sustainable crop growth
+        </p>
+      </div>
+
+      <button className="retrieve-button">retrieve results</button>
+    </>
+  )
+}
